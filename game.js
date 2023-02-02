41ca8727a01f137dd92add53ba8b8f6119d187fe
Ismet Medjebeur <i.medjebeur@live.fr>
2023-02-02 15:49:48 +0100

    everything fixed except return loseHealth

--- game.js
+++ game.js
@@ -19,27 +19,27 @@ const endLevelAudio = document.getElementById("end-level-audio");
 const endGameAudio = document.getElementById("end-game-audio");
 const loseLifeAudio = document.getElementById("lose-life-audio");
 const bombAudio = document.getElementById("bomb-audio");
-const mute = document.getElementById("mute")
+const mute = document.getElementById("mute");
 
 const colums = 10;
 const rows = 10;
 const cellsClass = [];
 const cellElements = [];
 let playerPosition = 95;
-let showScore = 0; //
-let aimScore = 0;
-let goalScore = 75;
+let showScore = 0; //is the score we are displaying on the page
+let aimScore = 0; //is a counter score so we know when we it the goalScore to get to the next level
+let goalScore = 75; //is the score needed to reach to get to the next level
 let health = 3;
 let intervalId = null;
 let timerInterval = null;
 let timeOutId = null;
 let speed = 500;
 let numberOfEnemies = 1;
-let pause = false;
+let pause = false; //boolean to help us stop the score and the time at the end of a level and start the new level with the score and time from the previous level
 let counterSeconds = 0;
 let counterMinutes = 0;
 let debug = Date.now();
-let nombre = 1;
+let nombre = 1; //a int to help us keep track of which level we are
 
 //Set the grid
 function createGrid() {
@@ -71,30 +71,22 @@ function hidePlayer() {
 function displayGrid() {
   displayPlayer();
   cellsClass.forEach((cell, i) => {
-    //Allow us to do the collision between the player and an obstacle
+    //Allow us to do the collision between the player and an obstacle/life or bomb
     if (i === playerPosition) {
       if (cell === "bad" && cell !== "life") {
         hidePlayer();
         health--;
         loseLifeMusic();
         showHealth();
-
-        // for (let i = 0; i < health ; i++){
-
-        // healthElement.innerHTML = ""
-        // for (boucle sur health) {
-        //   append coeur
-        // }
-        // healthElement.textContent = health;
       }
+
       if (cell === "life") {
         health++;
         showHealth();
-        // console.log(health);
-        // healthElement.textContent = health;
       }
+      //if nombre >=3 means if level 3 or more
       if (nombre >= 3 && cell === "bomb") {
-        cellsClass.splice(0, cellsClass.length);
+        cellsClass.splice(0, cellsClass.length); //clear the grid from all obstacle
         createGrid();
         bombMusic();
         showScore += 50;
@@ -104,7 +96,7 @@ function displayGrid() {
       }
 
       //console.log("Collision !");
-      return loseHealth();
+      return loseHealth(); //if i comment that line my player position hide and appear only when i move !!!!
     }
     cellElements[i].className = `cell ${cell}`;
   });
@@ -112,40 +104,34 @@ function displayGrid() {
 
 //Create enemies on the top line
 function createEnemies(number) {
-  //   const copy = [...cellElements];
-  //   copy.splice(playerPosition, 1);
-  //   copy.splice(90, 10);
-  //   for (let i = 0; i < number; i++) {
-  //     const randomIndex = Math.floor(Math.random() * copy.length);
-  //     const randomCell = copy[randomIndex];
-  //     randomCell.classList.add("bad");
-  //     copy.splice(randomIndex, 1);
-  //  }
-
   //create an new array of 10, then the splice remove the 10 last element
   //of the array cells & then we add the new array of 10 at the beginning
   //of the cells array, with new enemies inside so we got every new line
   //some enemies and allow us to made the enemies go down
-  const newLineEnemies = new Array(10).fill("");
+  const newLineEnemies = new Array(10).fill(""); //create an fill a new array of 10
   let randomNumber = Math.floor(Math.random() * newLineEnemies.length);
   if (health < 2 && randomNumber > 8) {
+    //create life obstacle to get an extra life
     const randomLife = Math.floor(Math.random() * newLineEnemies.length);
     newLineEnemies[randomLife] = "life";
   } else if (nombre >= 3 && randomNumber >= 9) {
+    //create bomb obstacle to clear the grid
     const randomBomb = Math.floor(Math.random() * newLineEnemies.length);
     newLineEnemies[randomBomb] = "bomb";
   }
   for (let i = 0; i < number; i++) {
+    //create classic obstacle
     const randomIndex = Math.floor(Math.random() * newLineEnemies.length);
 
     newLineEnemies[randomIndex] = "bad";
   }
-  cellsClass.splice(-10);
-  cellsClass.unshift(...newLineEnemies);
+  cellsClass.splice(-10); //erase the last 10 element of the array cells
+  cellsClass.unshift(...newLineEnemies); //add a new 10 element a the beginning of the array cells
 }
 createGrid();
 //displayScore();
 
+//function create the heart class to display the number of lives remaining to the player
 function showHealth() {
   healthElement.innerHTML = null;
 
@@ -156,10 +142,11 @@ function showHealth() {
   }
 }
 
+//evrything is in the name ;)
 function startGame(speed) {
   // console.log("from start game");
-  clearInterval(intervalId);
-  clearInterval(timerInterval);
+  clearInterval(intervalId); //interval de défilement des obstacle
+  clearInterval(timerInterval); // interval de temps
   intervalId = null;
   showHealth();
   // score =0;
@@ -167,20 +154,16 @@ function startGame(speed) {
   modalEndLevel.close();
   // modalLoseLife.close();
   displayTime();
-  levelMoment.textContent = nombre;
+  levelMoment.textContent = nombre; // affiche le niveau auquel on est
   let i = 0;
   startMusic();
 
   intervalId = setInterval(() => {
-    // console.log(Date.now() - debug);
-    // debug = Date.now();
-    // console.log("hellooooooo");
-    // console.log(speed);
     createEnemies(numberOfEnemies);
     displayGrid();
+    //allow us to set time to display the score once the first obstacle reach the baseline
     i += speed / 10;
     if (i > speed) {
-      // console.log(i);
       displayScore();
     }
     endLevel();
@@ -221,17 +204,14 @@ function getRandomArbitrary() {
 
 //add the random score calculated up to the score and display it every X seconds
 function displayScore() {
-  // timeOutId =setTimeout(() => {
-  // console.log(pause);
   if (!pause) {
     showScore += getRandomArbitrary();
   }
-
   aimScore = showScore;
   score.textContent = showScore;
-  // }, 10000);
 }
 
+//
 function displayFinalScore() {
   finalScore.forEach((el) => (el.textContent = showScore));
 }
@@ -239,8 +219,6 @@ function displayFinalScore() {
 //allow us to show a dialog message to say the level is finished after hitting
 //a certain score
 function endLevel() {
-  // console.log(speed);
-
   if (health <= 0) {
     endGame();
   } else {
@@ -250,22 +228,22 @@ function endLevel() {
       stopPlayMusic();
       endLevelMusic();
       modalEndLevel.showModal();
-      // playerPosition = 95;
+      playerPosition = 95;
       clearInterval(intervalId);
-      // clearInterval(timerInterval);
       cellsClass.splice(0, cellsClass.length);
       displayFinalScore();
       goalScore *= 3;
       speed -= 50;
-      // console.log(speed);
       numberOfEnemies++;
       nombre++;
     }
   }
 }
+
+//next level button and what happen when we clikc on it
 nextLevel.addEventListener("click", () => {
   createGrid();
-  aimScore = 0;
+  aimScore = 0; // reset aimScore to 0 (it's our counter) so we can play until we reach goalScore
   displayGrid();
   pause = false;
   stopEndLevelMusic();
@@ -281,9 +259,10 @@ function endGame() {
   clearInterval(timerInterval);
   cellsClass.splice(0, cellsClass.length);
   displayFinalScore();
-
-  // clearTimeout(timeOutId);
 }
+
+//next level button and what happen when we clikc on it
+//stop endGameMusic, reset eveything and star a new game
 resetGame.addEventListener("click", () => {
   stopEndGameMusic();
   showScore = 0;
@@ -314,6 +293,7 @@ function loseHealth() {
   // });
 }
 
+//everything is in the title function
 function displayTime() {
   timerInterval = setInterval(() => {
     if (!pause) {
@@ -335,16 +315,18 @@ function displayTime() {
   }, 1000);
 }
 
+//everything is in the title function
 function displayLevelNumber(nombre) {
   levelNumber.textContent = nombre;
   nextLevelNumber.textContent = nombre + 1;
 }
 
+// the 10 next function are the audio setting for the game, endgame, endlevel, loselife and bomb
 function startMusic() {
   // const audio = new Audio('./sounds/play-mode.mp3');
   // audio.addEventListener('canplaythrough', () => audio.play())
   playMode.playbackRate = 1.5;
-  playMode.loop = true
+  playMode.loop = true;
   playMode.play();
 }
 
@@ -393,20 +375,19 @@ function stopBombMusic() {
   bombAudio.pause();
 }
 
+//button to start a game and start it only once => once:true
 buttonStart.addEventListener("click", () => startGame(speed), { once: true });
 
+//mute button to shut down all music
 mute.addEventListener("click", () => {
   stopPlayMusic();
   stopEndLevelMusic();
   stopEndGameMusic();
   endLoseLifeMusic();
   stopBombMusic();
-
 });
 
 // resetGame.addEventListener("click", () => {
 //   createGrid();
 //   // startGame(500);
 // });
-
-
